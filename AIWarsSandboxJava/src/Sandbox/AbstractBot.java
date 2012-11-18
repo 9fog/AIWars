6b001fd8861faca1e2726c57f9bb5c5ef65c2908
nazymko <nazymko.patronus@gmail.com>
2012-11-18 22:05:16 +0200

    some questions

--- AIWarsSandboxJava/src/Sandbox/AbstractBot.java
+++ AIWarsSandboxJava/src/Sandbox/AbstractBot.java
@@ -3,8 +3,12 @@ package Sandbox;
 import java.util.ArrayList;
 
 public abstract class AbstractBot {
-	public abstract void processInit(Simulator sim, 
-			                int mapSizeX, int mapSizeY, 
-			                ArrayList<ObjectRock> rocks, ArrayList<ObjectFlag> flags, ArrayList<ObjectUnit> units);
-	public abstract void processTick(ArrayList<Object> events);
+    public abstract void processInit(Simulator sim,
+                                     int mapSizeX,
+                                     int mapSizeY,
+                                     ArrayList<ObjectRock> rocks,
+                                     ArrayList<ObjectFlag> flags,
+                                     ArrayList<ObjectUnit> units);
+
+    public abstract void processTick(ArrayList<Object> events);
 }
--- AIWarsSandboxJava/src/Sandbox/Connector.java
+++ AIWarsSandboxJava/src/Sandbox/Connector.java
@@ -1,4 +1,5 @@
 package Sandbox;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -9,172 +10,162 @@ import java.util.Calendar;
 
 import net.minidev.json.JSONObject;
 
-public class Connector
-{
-  private final Socket _socket;
-  private final BufferedReader socketReader;
-  private final BufferedWriter socketWriter;
-  private final Connector.SocketWrapper _wrapper;
-  private final Simulator _simulator;
+public class Connector {
+    private final Socket _socket;
+    private final BufferedReader socketReader;
+    private final BufferedWriter socketWriter;
+    private final Connector.SocketWrapper _wrapper;
+    private final Simulator _simulator;
 
-  public Connector()
-    throws IOException
-  {
-     this._socket = new Socket("192.168.1.95", 23001);
+    public Connector()
+            throws IOException {
+        this._socket = new Socket("192.168.1.95", 23001);
 
-     this.socketReader = new BufferedReader(new InputStreamReader(this._socket.getInputStream(), "UTF-8"));
-     this.socketWriter = new BufferedWriter(new OutputStreamWriter(this._socket.getOutputStream(), "UTF-8"));
+        this.socketReader = new BufferedReader(new InputStreamReader(this._socket.getInputStream(), "UTF-8"));
+        this.socketWriter = new BufferedWriter(new OutputStreamWriter(this._socket.getOutputStream(), "UTF-8"));
 
-     new Thread(new Connector.Receiver()).start();
-     this._wrapper = new Connector.SocketWrapper();
-     this._wrapper.start();
+        new Thread(new Connector.Receiver()).start();
+        this._wrapper = new Connector.SocketWrapper();
+        this._wrapper.start();
 
-     this._simulator = new Simulator(this);
-     
-     //Send initialization command
-     JSONObject cmd = new JSONObject();
-     cmd.put("_op", "testFight");
-     cmd.put("botsCount", Config.BOTS.length);
-     cmd.put("mapName", Config.MAP_NAME);
-     cmd.put("maxTicks", Config.COMBAT_MAX_TICKS);
-     send (cmd.toJSONString());
-  }
+        this._simulator = new Simulator(this);
 
-  public void send(String msg)
-  {
-     this._wrapper.addInQueue(msg);
-     log("SND > " + msg);
-  }
+        //Send initialization command
+        JSONObject cmd = new JSONObject();
+        cmd.put("_op", "testFight");
+        cmd.put("botsCount", Config.BOTS.length);
+        cmd.put("mapName", Config.MAP_NAME);
+        cmd.put("maxTicks", Config.COMBAT_MAX_TICKS);
+        send(cmd.toJSONString());
+    }
 
-  public synchronized void close()
-  {
-     if (!this._socket.isClosed())
-      try {
-         this._socket.close();
-         System.exit(0);
-      } catch (IOException ignored) {
-         ignored.printStackTrace();
-      }
-  }
+    public void send(String msg) {
+        this._wrapper.addInQueue(msg);
+        log("SND > " + msg);
+    }
 
-  public static void log(String activity)
-  {
-     Calendar cal = Calendar.getInstance();
-     activity = "[" + cal.get(5) + 
-       "." + (cal.get(2) + 1) + 
-       " " + 
-       cal.get(11) + 
-       ":" + cal.get(12) + 
-       ":" + cal.get(13) + 
-       "] " + activity;
-     activity = activity + "\n";
-
-     System.out.print(activity);
-  }
-
-  private class Receiver extends Thread
-  {
-    public void run()
-    {
-       Connector.log("Reciever started...");
-       while (!Connector.this._socket.isClosed())
-        try {
-           char[] charBuffer = new char[4096];
-
-           while (Connector.this.socketReader.read(charBuffer, 0, 1) != -1) {
-             StringBuilder sb = new StringBuilder();
-             String data = "";
-
-             while ((charBuffer[0] != 0) && (sb.length() < 4096)) {
-               sb.append(charBuffer[0]);
-               Connector.this.socketReader.read(charBuffer, 0, 1);
+    public synchronized void close() {
+        if (!this._socket.isClosed())
+            try {
+                this._socket.close();
+                System.exit(0);
+            } catch (IOException ignored) {
+                ignored.printStackTrace();
             }
-
-             data = sb.toString();
-             Connector.log("RCV > " + data);
-
-             Connector.this._simulator.processServerCommand(data);
-
-             sleep(100);
-          }
-        }
-        catch (IOException e) {
-           if ("Socket closed".equals(e.getMessage())) {
-            break;
-          }
-           Connector.log("Connection lost");
-           Connector.this.close();
-        } catch (Exception e) {
-           e.printStackTrace();
-        } 
     }
-  }
 
-  private class SocketWrapper extends Thread {
-     public int qStartPoint = 0;
-     public int qStopPoint = 0;
-     public int qMaxIndex = 500;
-     public String[] Queue = new String[qMaxIndex + 1];
-     private String message;
+    public static void log(String activity) {
+        //suggest duplicate log to file on hard drive
+        Calendar cal = Calendar.getInstance();
+        activity = "[" + cal.get(5) +
+                "." + (cal.get(2) + 1) +
+                " " +
+                cal.get(11) +
+                ":" + cal.get(12) +
+                ":" + cal.get(13) +
+                "] " + activity;
+        activity = activity + "\n";
 
-     public void run() { 
-     	try { 
-     		Connector.log("Transmitter started");
-         while (true) {
-           while (qStartPoint!=qStopPoint){   //Если указатели не равны, значит есть что отсылать
-        	   synchronized (Queue) {
-        		   message = Queue[qStartPoint];
-        		   Queue[qStartPoint] = null;
-        	   }
-        	   try {
-        		   socketWriter.write(message + "\0");
-        		   socketWriter.flush();
-        	   }
-        	   catch (Exception e) {
-        		   e.printStackTrace();
-        		   close();
-        	   }
+        System.out.print(activity);
+    }
 
-        	   qStartPoint += 1;
-        	   if (qStartPoint > qMaxIndex) {
-        		   qStartPoint = 0;
-        	   }           
-           
-        	   try
-        	   {
-        		   sleep(100);
-        	   } catch (InterruptedException e) {
-        		   e.printStackTrace();
-        	   }
-           }
-           
-            while (qStartPoint == qStopPoint) {
-             try {
-                sleep(100);
-             } catch (InterruptedException e) {
+    private class Receiver extends Thread {
+        public void run() {
+            Connector.log("Reciever started...");
+            while (!Connector.this._socket.isClosed())
+                try {
+                    char[] charBuffer = new char[4096];
+
+                    while (Connector.this.socketReader.read(charBuffer, 0, 1) != -1) {   //one question - WTF?
+                        StringBuilder sb = new StringBuilder();
+                        String data = "";
+
+                        while ((charBuffer[0] != 0) && (sb.length() < 4096)) {
+                            sb.append(charBuffer[0]);
+                            Connector.this.socketReader.read(charBuffer, 0, 1);//not clear logic
+                        }
+
+                        data = sb.toString();
+                        Connector.log("RCV > " + data);
+
+                        Connector.this._simulator.processServerCommand(data);
+
+                        sleep(100);//magic constant, and why sleep thread?
+                    }
+                } catch (IOException e) {
+                    if ("Socket closed".equals(e.getMessage())) {
+                        break;
+                    }
+                    Connector.log("Connection lost");
+                    Connector.this.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+        }
+    }
+
+    private class SocketWrapper extends Thread {
+        public int qStartPoint = 0;
+        public int qStopPoint = 0;
+        public int qMaxIndex = 500;
+        public String[] Queue = new String[qMaxIndex + 1];
+        private String message;
+
+        public void run() {
+            try {
+                Connector.log("Transmitter started");
+                while (true) {
+                    while (qStartPoint != qStopPoint) {   //Если указатели не равны, значит есть что отсылать
+                        synchronized (Queue) {
+                            message = Queue[qStartPoint];
+                            Queue[qStartPoint] = null;
+                        }
+                        try {
+                            socketWriter.write(message + "\0");
+                            socketWriter.flush();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            close();
+                        }
+
+                        qStartPoint += 1;
+                        if (qStartPoint > qMaxIndex) {
+                            qStartPoint = 0;
+                        }
+
+                        try {
+                            sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    while (qStartPoint == qStopPoint) {
+                        try {
+                            sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            } catch (NullPointerException e) {
                 e.printStackTrace();
-             }
-           }
-         } 
-       } catch (NullPointerException e) {
-          e.printStackTrace();
-       } 
-    }
-
-    public void addInQueue(String d)
-    {
-       synchronized (this) {
-         Queue[qStopPoint] = d;
-         qStopPoint ++;
-         if (qStopPoint > qMaxIndex) {
-           qStopPoint = 0;
-         }
-         
-         if (qStartPoint == qStopPoint) {
-           Connector.log("Error: wrapper owerflow");
-           close();
+            }
         }
-      }
+
+        public void addInQueue(String d) {
+            synchronized (this) {
+                Queue[qStopPoint] = d;
+                qStopPoint++;
+                if (qStopPoint > qMaxIndex) {
+                    qStopPoint = 0;
+                }
+
+                if (qStartPoint == qStopPoint) {
+                    Connector.log("Error: wrapper owerflow");
+                    close();
+                }
+            }
+        }
     }
-  }
 }
